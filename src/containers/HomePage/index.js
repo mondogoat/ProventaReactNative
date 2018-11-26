98a494b7d281e1660943025689284eff3a64ade9
aeonfaux <tonimaristelacarpio@gmail.com>
2018-11-26 16:27:40 +0800

    HomePage Event List and Item Rendering

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,9 +1,95 @@
 import React, { Component } from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, ScrollView, Image } from "react-native";
 import { Header, TabbedMenu, Card } from "../../components";
 import PageStyle from "./styles";
 
 class HomePage extends Component {
+  state = {
+    meetings: [
+      {
+        id: 1,
+        region: "APAC",
+        events: [
+          {
+            id: "apac1",
+            title: "https://i.ibb.co/QYRP3H7/event-description-1.png",
+            description: "HR Leaders Strategy Meeting APAC",
+            event: "Singapore 2018"
+          },
+          {
+            id: "apac2",
+            title: "https://i.ibb.co/QYRP3H7/event-description-1.png",
+            description: "HR Leaders Strategy Meeting APAC",
+            event: "Malaysia 2018"
+          },
+          {
+            id: "apac3",
+            title: "https://i.ibb.co/QYRP3H7/event-description-1.png",
+            description: "HR Leaders Strategy Meeting APAC",
+            event: "Hongkong 2018"
+          }
+        ]
+      },
+      {
+        id: 2,
+        region: "EUROPE",
+        events: [
+          {
+            id: "europe1",
+            title: "https://i.ibb.co/QYRP3H7/event-description-1.png",
+            description: "HR Leaders Strategy Meeting APAC",
+            event: "France 2018"
+          },
+          {
+            id: "europe2",
+            title: "https://i.ibb.co/QYRP3H7/event-description-1.png",
+            description: "HR Leaders Strategy Meeting APAC",
+            event: "Germany 2018"
+          },
+          {
+            id: "europe",
+            title: "https://i.ibb.co/QYRP3H7/event-description-1.png",
+            description: "HR Leaders Strategy Meeting APAC",
+            event: "Italy 2018"
+          }
+        ]
+      }
+    ]
+  };
+
+  renderCategories() {
+    const category = this.state.meetings.map(({ id, region, events }) => {
+      return (
+        <View key={id} style={PageStyle.eventList}>
+          <Text style={PageStyle.category}> {region}</Text>
+          <View style={PageStyle.eventContainer}>
+            {this.renderEvents(events)}
+          </View>
+          <View style={PageStyle.expandButton}>
+            <Text> More Meetings </Text>
+          </View>
+        </View>
+      );
+    });
+
+    return category;
+  }
+
+  renderEvents(events) {
+    const meeting = events.map(({ id, title, description, event }) => {
+      return (
+        <View key={id}>
+          <Image style={PageStyle.eventTitle} source={{ uri: title }} />
+          <Text style={PageStyle.eventDescription}> {description}</Text>
+          <Text style={PageStyle.eventDate}> {event}</Text>
+          <View style={PageStyle.eventBorder} />
+        </View>
+      );
+    });
+
+    return meeting;
+  }
+
   render() {
     const { navigation } = this.props;
     return (
@@ -12,20 +98,23 @@ class HomePage extends Component {
           label="Strategy Meetings"
           onPress={() => navigation.openDrawer()}
         />
-        <Card>
-          <Image
-            style={PageStyle.image}
-            source={require("../../assets/event_1.png")}
-          />
-          <View style={PageStyle.info}>
-            <Text style={PageStyle.description}>
-              Digital Transformation Strategy Meeting APAC - Manila 2018
-            </Text>
-            <Text style={PageStyle.date}>
-              22nd November 2018 | Marriot Grand Ballroom - Manila
-            </Text>
-          </View>
-        </Card>
+        <ScrollView>
+          <Card>
+            <Image
+              style={PageStyle.image}
+              source={require("../../assets/event_1.png")}
+            />
+            <View style={PageStyle.info}>
+              <Text style={PageStyle.description}>
+                Digital Transformation Strategy Meeting APAC - Manila 2018
+              </Text>
+              <Text style={PageStyle.date}>
+                22nd November 2018 | Marriot Grand Ballroom - Manila
+              </Text>
+            </View>
+          </Card>
+          {this.renderCategories()}
+        </ScrollView>
         <TabbedMenu navigation={navigation} />
       </View>
     );
--- src/containers/HomePage/styles.js
+++ src/containers/HomePage/styles.js
@@ -6,8 +6,13 @@ const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 export default (PageStyle = {
   container: {
-    flex: 1,
-    alignItems: "center"
+    flex: 1
+  },
+  category: {
+    fontSize: 20,
+    color: DARK_GRAY,
+    fontWeight: "500",
+    paddingBottom: SCREEN_HEIGHT * 0.01
   },
   image: {
     width: "95%",
@@ -30,5 +35,48 @@ export default (PageStyle = {
     fontSize: 12,
     color: GRAY,
     paddingBottom: SCREEN_HEIGHT * 0.01
+  },
+  expandButton: {
+    padding: SCREEN_WIDTH * 0.025,
+    paddingTop: SCREEN_HEIGHT * 0.035,
+    paddingBottom: SCREEN_HEIGHT * 0.035,
+    width: "100%",
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    flex: 1
+  },
+  eventContainer: {
+    backgroundColor: "#ffffff"
+  },
+  eventList: {
+    padding: SCREEN_WIDTH * 0.025,
+    width: "100%",
+    alignItems: "flex-start"
+  },
+  eventTitle: {
+    width: SCREEN_WIDTH * 0.95,
+    height: SCREEN_HEIGHT * 0.07,
+    resizeMode: "contain"
+  },
+  eventDescription: {
+    fontSize: 15,
+    color: DARK_GRAY,
+    paddingTop: SCREEN_HEIGHT * 0.01,
+    paddingLeft: SCREEN_WIDTH * 0.02,
+    backgroundColor: "#ffffff"
+  },
+  eventDate: {
+    fontSize: 15,
+    color: DARK_GRAY,
+    paddingBottom: SCREEN_HEIGHT * 0.03,
+    paddingLeft: SCREEN_WIDTH * 0.02,
+    backgroundColor: "#ffffff"
+  },
+  eventBorder: {
+    borderColor: GRAY,
+    borderWidth: SCREEN_HEIGHT * 0.001,
+    width: "95%",
+    alignSelf: "center",
+    opacity: 0.3
   }
 });
