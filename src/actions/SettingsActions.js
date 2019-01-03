e13162d35b72d4b4e20fb73437041be038561bb2
aeonfaux <tonimaristelacarpio@gmail.com>
2019-01-03 14:58:59 +0800

    Refactored Settings Actions - Created separate functions for retrieving calendar settings and notification settings
    - Created separate update functions for retrieving calendar settings and notification settings
    
    - Corrected type actions used for successful and failed http requests

--- src/actions/SettingsActions.js
+++ src/actions/SettingsActions.js
@@ -1,26 +1,48 @@
 import {
   SETTINGS_UPDATE,
-  FETCH_SETTINGS,
+  FETCH_CALENDAR_SETTINGS,
+  FETCH_NOTIFICATION_SETTINGS,
   SETTINGS_CONFIG_SUCCESS,
   SETTINGS_CONFIG_FAIL
 } from "./types";
 
 //Update syncGoogle and syncCalendar field
+//Update push, sms, or email
 export const updateSettings = ({ prop, value }) => {
   return {
-    type: SSETTINGS_UPDATE,
+    type: SETTINGS_UPDATE,
     payload: { prop, value }
   };
 };
 
-//Retrieve settings
-export const fetchSettings = () => {
+//Retrieve calendar settings
+export const fetchCalendarSettings = userId => {
   try {
-    const request = await axios.GET(`${SERVER_ADDRESS}/settings`);
+    const request = await axios.GET(
+      `${SERVER_ADDRESS}/${userId}/settings/calendar`
+    );
 
     if (request.status === "SUCCESS") {
       dispatch({
-        type: FETCH_SETTINGS,
+        type: FETCH_CALENDAR_SETTINGS,
+        payload: request.data
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Retrieve notification settings
+export const fetchNotificationSettings = userId => {
+  try {
+    const request = await axios.GET(
+      `${SERVER_ADDRESS}/${userId}/settings/notification`
+    );
+
+    if (request.status === "SUCCESS") {
+      dispatch({
+        type: FETCH_NOTIFICATION_SETTINGS,
         payload: request.data
       });
     }
@@ -30,21 +52,52 @@ export const fetchSettings = () => {
 };
 
 //Update Settings
-export const updateProfile = (form, callback) => {
+export const updateCalendarSettings = (form, callback) => {
   try {
-    const request = await axios.PATCH(`${SERVER_ADDRESS}/settings`, {
-      syncGoogle: form.syncGoogle,
-      syncCalendar: form.syncCalendar
-    });
+    const request = await axios.PATCH(
+      `${SERVER_ADDRESS}/${form.userId}/settings/calendar`,
+      {
+        syncGoogle: form.syncGoogle,
+        syncCalendar: form.syncCalendar
+      }
+    );
 
     if (request.status === "SUCCESS") {
       dispatch({
-        type: PROFILE_UPDATE_SUCCESS,
+        type: SETTINGS_CONFIG_SUCCESS,
         payload: "Settings Update Successful"
       });
     } else {
       dispatch({
-        type: PROFILE_UPDATE_FAIL,
+        type: SETTINGS_CONFIG_FAIL,
+        payload: "Settings Update Failed"
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Update NotificationSettings
+export const updateNotificationSettings = (form, callback) => {
+  try {
+    const request = await axios.PATCH(
+      `${SERVER_ADDRESS}/${form.userId}/settings/notification`,
+      {
+        push: form.syncGoogle,
+        sms: form.syncCalendar,
+        email: form.email
+      }
+    );
+
+    if (request.status === "SUCCESS") {
+      dispatch({
+        type: SETTINGS_CONFIG_SUCCESS,
+        payload: "Settings Update Successful"
+      });
+    } else {
+      dispatch({
+        type: SETTINGS_CONFIG_FAIL,
         payload: "Settings Update Failed"
       });
     }
