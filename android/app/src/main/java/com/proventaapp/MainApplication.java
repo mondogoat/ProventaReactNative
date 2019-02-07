bf0274a84d0d3f8b437b9ef12c2a8ffddfe5451b
aeonfaux <tonimaristelacarpio@gmail.com>
2019-02-07 14:18:50 +0800

    Reconfigured Push Notifications(forIOS) with Amplify, Mobile and Pinpoint

--- android/app/src/main/java/com/proventaapp/MainApplication.java
+++ android/app/src/main/java/com/proventaapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.proventaapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.amazonaws.amplify.pushnotification.RNPushNotificationPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.calendarevents.CalendarEventsPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNPushNotificationPackage(),
             new ReactNativeOneSignalPackage(),
             new CalendarEventsPackage(),
             new RNCameraPackage(),
